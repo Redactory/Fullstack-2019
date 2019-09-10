26a4acd8dd827d1e17598a05821106e7551d23c2
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-09-10 14:37:33 +0300

    Tehtävä 1.11 toteutus

--- Osa1/unicafe/src/components/Statistic.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function(props) {
-    return <p>{props.text} {props.value}</p>
-}
--- Osa1/unicafe/src/components/Statistics.js
+++ Osa1/unicafe/src/components/Statistics.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import Statistic from './Statistic';
+import React from 'react'
 
-export default function Statistics(props) {
-    const clicks = props.good + props.neutral + props.bad;
-    if (clicks === 0) {
-        return <p>No feedback given</p>;
-    }
-    
-    return (
-        <div>
-            <h1>statistics</h1>
-            <Statistic text="good" value={props.good}/>
-            <Statistic text="neutral" value={props.neutral}/>
-            <Statistic text="bad" value={props.bad}/>
-            <Statistic text="all" value={props.good + props.neutral + props.bad}/>
-            <Statistic text="average" value={props.average(props.good, props.neutral, props.bad)}/>
-            <Statistic text="positive" value={props.positives(props.good, props.neutral, props.bad)}/>
-      </div>
-    );
+export default function Statistics (props) {
+  const clicks = props.good + props.neutral + props.bad
+  if (clicks === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{props.good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{props.neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{props.bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{props.good + props.neutral + props.bad}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{props.average(props.good, props.neutral, props.bad)}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{props.positives(props.good, props.neutral, props.bad)}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
