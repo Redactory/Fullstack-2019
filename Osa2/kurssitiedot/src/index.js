52a7409f7819d7c18157de98228932842e0d9531
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-09-13 11:45:19 +0300

    Tehtävien 2.4 & 2.5 toteutukset
    Course-osa on alusta pitäen ollut omana moduulina mutta merkataan sen teko
    osaksi tätä kommittia...

--- Osa2/kurssitiedot/src/index.js
+++ Osa2/kurssitiedot/src/index.js
@@ -4,35 +4,57 @@ import ReactDOM from 'react-dom';
 import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Added component',
-        exercises: 4,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  const courseListing = [];
+    for (let i=0; i<courses.length; i++) {
+      courseListing.push(<Course key={courses[i].name} course={courses[i]} />);
+    }
 
   return (
     <div>
-      <Course course={course} />
+      {courseListing}
     </div>
   )
 }
