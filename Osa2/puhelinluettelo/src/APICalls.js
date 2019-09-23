c9d96119dd978d7bc6cd024480004a12e03a02ec
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-09-23 11:35:09 +0300

    Tehtävän 2.16 toteutus

--- Osa2/puhelinluettelo/src/APICalls.js
+++ Osa2/puhelinluettelo/src/APICalls.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 const baseUrl = 'http://localhost:3001/persons';
 
+function populatePersonListState(setPersons) {
+    const request = axios.get(baseUrl);
+    return request.then(response => setPersons(response.data));
+}
+
 function newPerson(newObject) {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
+    const request = axios.post(baseUrl, newObject);
+    return request.then(response => response.data);
 }
 
 export default {
+    populatePersonListState: populatePersonListState,
     newPerson: newPerson
 };
 
--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios';
 
 import PersonContainer from './components/PersonContainer';
 import SearchBar from './components/SearchBar';
@@ -13,11 +12,7 @@ const App = () => {
   const [ search, setSearch ] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data);
-      })
+    APICalls.populatePersonListState(setPersons);
   }, [])
 
   function addPerson(event) {
